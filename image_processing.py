48d31ecfa6f87d2ddb99be9208373d1f2ae008c7
madisonhb <59774711+madisonhb@users.noreply.github.com>
2022-01-05 13:09:04 -0800

    Define functions for project pipeline

---
+++ image_processing.py
@@ -0,0 +1,11 @@
+'''Implement image processing functions
+
+Get images from Hubble Legacy Archive in fetch_images() and implement bilateral
+filter and histogram equalization in process_data().
+'''
+
+def fetch_images():
+    #TODO: Implement. (Noah)
+
+def process_data():
+    #TODO: Implement. (Madison)
---
+++ main.py
@@ -0,0 +1,7 @@
+# Start pipeline for image preprocessing.
+
+if __name__ = "__main__":
+    fetch_images()
+    process_data()
+    train_network()
+    # TODO: Save network.
